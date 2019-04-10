ee6b26c5525cd0576f789241f026a8ab0bdcd2ed
weijia0208 <10024752762@qq.com>
2019-04-10 17:03:27 +0800

    关于对话框

---
+++ jsnote.js
@@ -0,0 +1,35 @@
+/*exported $jsNote*/
+var $jsNote = (function(){
+  var DOM = '<div class="container">'
+            +'<div class="top">'
+            +'<p class="title">关于“记事本”</p>'
+            +'<span class="close">×</span>'
+            +'</div>'
+            +'<div class="content">'
+            +'<h1 class="jsnote">JSNotepad</h1>'
+            +'<hr>'
+            +'<div class="text">'
+              +'<p>作者：佳佳</p>'
+              +'<p>QQ：1002475276</p>'
+              +'<p>仓库地址：<a href="https://github.com/weijia0208/jsnotepad" target="_blank">https://github.com/weijia0208/jsnotepad</a></p>'
+            +'</div>'
+            +'<input type="button" value="确定" class="del">'
+            +'</div>'
+            +'</div>';
+  
+  var $jnp = $(DOM),
+      $close = $jnp.find('.close'),
+      $del = $jnp.find('.del');
+
+  var show = function(){
+    $('body').append($jnp);
+
+    $close.click(function(){
+      $jnp.remove();
+    });
+    $del.click(function(){
+      $jnp.remove();
+    });
+  };
+  return {show:show};
+}());
